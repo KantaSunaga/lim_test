27addbfa1fb27c4f1def5031f0af2c2780ea6e6a
root <root@ip-172-31-46-65.ap-northeast-1.compute.internal>
2018-10-29 12:36:32 +0000

    idとシークレットキー間違ってた

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,9 +1,11 @@
+user = "sunagakanntawork@gmail.com"
+password = "nzijttetoytlzcfw"
 ActionMailer::Base.smtp_settings = {
   address: 'smtp.gmail.com',
   domain: 'smtp.gmail.com',
   port: 587,
-  user_name: 'sunagakanntawork@gmail.com',
-  password: ENV["GMAILE_SECRET_KEY"] ,
+  user_name: user,
+  password: password,
   authentication: 'login',
   enable_starttls_auto: true
 }
